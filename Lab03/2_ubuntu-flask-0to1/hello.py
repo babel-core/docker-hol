304efcdbb386a2579928a3f13e531b4579ea98e9
byjeon <mysummit@gmail.com>
2017-12-14 21:38:00 +0900

    hol reorder and rearrange

--- Lab03/2_ubuntu-flask-0to1/hello.py
+++ Lab03/2_ubuntu-flask-0to1/hello.py
@@ -1,4 +1,5 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/hi')
@@ -6,5 +7,4 @@ def hello_world():
     return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
-
+    app.run(host='0.0.0.0', port=5000)
